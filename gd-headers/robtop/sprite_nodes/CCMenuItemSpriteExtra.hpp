a92b6f643b8bb71a2d6ce828a9e6df94acb898cf
cierra-kb <174018532+cierra-kb@users.noreply.github.com>
2025-09-23 01:35:49 +0800

    Add CCMenuItemSpriteExtra header

---
+++ gd-headers/robtop/sprite_nodes/CCMenuItemSpriteExtra.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cocos2d.h>
+
+class CCMenuItemSpriteExtra : public cocos2d::CCMenuItemSprite {
+public:
+    virtual ~CCMenuItemSpriteExtra();
+
+    virtual void activate();
+    virtual void selected();
+    virtual void unselected();
+
+    CC_SYNTHESIZE(float, m_fScaleVar, ScaleVar);
+    CC_SYNTHESIZE(float, m_fOriginalScale, OriginalScale);
+    CC_SYNTHESIZE(bool, m_bShouldAnimate, ShouldAnimate);
+    CC_SYNTHESIZE(bool, m_bDarkenClick, DarkenClick);
+    CC_SYNTHESIZE(float, m_fVolume, Volume);
+    CC_SYNTHESIZE(const char*, m_pClickSound, ClickSound);
+
+    static CCMenuItemSpriteExtra* create(
+        cocos2d::CCNode* sprite,
+        cocos2d::CCNode* unknown,
+        cocos2d::CCObject* target,
+        SEL_MenuHandler callback
+    );
+};
